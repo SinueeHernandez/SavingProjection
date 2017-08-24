129ebbe9b0466968f7b779ab825d44e14e806815
Sinuee Hernández <sinuee@gmail.com>
2017-08-24 18:01:49 -0500

    Internazionalitation
    I18n support for locale language traductions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  #Provide Locale to theapp
+  before_action :set_locale
+  
+  def set_locale
+   I18n.locale = params[:locale] || I18n.default_locale
+  end
+  
   protect_from_forgery with: :exception
+
 end
