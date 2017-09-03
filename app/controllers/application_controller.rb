9cf05ee6c05313396a4d8e88936da9b464882ace
Sinuee Hernández <sinuee@gmail.com>
2017-09-02 23:11:13 -0500

    Internazionalitation on route
    Now we can just add the language like http://localhost:3000/en

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
+  def default_url_options 
+    { locale: I18n.locale } 
+  end 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   
    root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope "(:locale)", locale: /en|nl/ do 
+    resources :welcome 
+  end 
+ 
+  get '/:locale' => 'welcome#index' 
+  
 end
