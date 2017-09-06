89e9d334aaa62c9e9c4d76e341fc9579ab05a6b2
Sinuee Hernández <sinuee@gmail.com>
2017-09-05 22:39:03 -0500

    Users creation
    User creation OK and session estatus is now handle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    attr_accessor  :password
+    #attr_accessor  :password
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, :uniqueness => true, format: { with: VALID_EMAIL_REGEX }
     validates :password, :confirmation => true  
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
    root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  scope "(:locale)", locale: /en|nl/ do 
+  scope "(:locale)", locale: /es|nl/ do 
     resources :welcome 
   end 
  
